29e8953e026946c88454bc45bd4fa7d55e30bc49
JGmadureira <joaogabrielsilvamadureira@gmail.com>
2022-03-23 15:50:12 -0300

    Tabela Dinâmica

--- Morangos/web/IFCIoTRTDB/js/IFCfertirriga.js
+++ Morangos/web/IFCIoTRTDB/js/IFCfertirriga.js
@@ -1,8 +1,9 @@
-var valveList = document.getElementById('valveList');
+//var valveList = document.getElementById('valveList');
+var valveList = [];
 var code = document.getElementById('code');
 var description = document.getElementById('description');
 var addButton = document.getElementById('addButton');
-var selButton = document.getElementById('selButton');
+var updButton = document.getElementById('updButton');
 var delButton = document.getElementById('delButton');
 var VAdb1flexSwitch = document.getElementById('VAdb1flexSwitch');
 var Vadb1label = document.getElementById('Vadb1label');
@@ -45,6 +46,8 @@ function AttDigitais(name, status) {
 
 
 function SelectAllDataFrom(colection){
+    //document.getElementById("tbody1").innerHTML="";
+
     firebase.database().ref(colection).once('value',
     function (AllRecords){
         AllRecords.forEach(
@@ -68,23 +71,58 @@ function AddItemsToTable(Nome,Descrição,Estado){
     var td2 = document.createElement('td');
     var td3 = document.createElement('td');
     var td4 = document.createElement('td');
+    var ControlDiv = document.createElement("td");
+    valveList.push([Nome, Descrição]);
+    
     td1.innerHTML = Nome;
     td2.innerHTML = Descrição;
-    td3.innerHTML = "botaozinho";
+    td3.innerHTML = 'botãozinho';
     td4.innerHTML = Estado;
+
+    ControlDiv.innerHTML = '<button type="button" class="btn btn-primary my-2" data-toggle="modal" data-target="#exampleModalCenter" onclick="FillTboxes(null)">Adicionar</button>'
+    ControlDiv.innerHTML += '<button type="button" class="btn btn-warning my-2 ml-2" data-toggle="modal" data-target="#exampleModalCenter" onclick="FillTboxes('+code+')">Editar</button>'
+    
     trow.appendChild(td1); 
     trow.appendChild(td2); 
     trow.appendChild(td3); 
     trow.appendChild(td4); 
 
-    var ControlDiv = document.createElement("div");
-    ControlDiv.innerHTML = '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">'
-    ControlDiv.innerHTML += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">'
+    
+    
 
     trow.appendChild(ControlDiv);
     tbody.appendChild(trow);
 
 }
+
+function FillTboxes(index){
+    if(index==null){
+        code.value = "";
+        description.value = "";
+        addButton.style.display='inline-block';
+        updButton.style.display='none';
+        delButton.style.display='none';
+    }
+    else{
+        --index;
+        code.value = valveList[index][1];
+        description.value = valveList[index][2];
+        addButton.style.display='none';
+        updButton.style.display='inline-block';
+        delButton.style.display='inline-block';
+    }
+
+    function addButton(){
+        firebase.database().ref("Digitais/"+code.value).set(
+        {
+            Descrição: description,
+            Estado: 'Desligado',
+            Nome: code
+        });
+    }
+
+}
+
 
 /*
 function Ready(){
