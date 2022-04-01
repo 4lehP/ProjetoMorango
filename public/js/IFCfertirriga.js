8716497edd4ed27ecce2360efe4fb31c205b2d9e
JGmadureira <joaogabrielsilvamadureira@gmail.com>
2022-04-01 15:14:19 -0300

    Site c/ senha

--- public/js/IFCfertirriga.js
+++ public/js/IFCfertirriga.js
@@ -93,7 +93,7 @@ function SelectAllDataToValveList(colection) {
 
 var senha = window.prompt("Insira a senha");
 
-if (senha == "inovamorago") {
+if (senha == 'inovamorago') {
     function AddItemsToTable(Nome, Descrição, Estado) {
         var tbody = document.getElementById('tbody');
         
